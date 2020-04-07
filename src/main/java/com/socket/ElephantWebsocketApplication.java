33ee554b71a08e3ccddd96629cabdb8cc74eea98
jiana <767664233@qq.com>
2020-04-07 22:42:15 +0800

    Initial commit

---
+++ src/main/java/com/socket/ElephantWebsocketApplication.java
@@ -0,0 +1,13 @@
+package com.socket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElephantWebsocketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElephantWebsocketApplication.class, args);
+    }
+
+}
